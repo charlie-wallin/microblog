5842a86e94861ec0107821794cf930b651a1ed6a
Charlie Wallin <charliekwallin@gmail.com>
2025-08-14 11:09:35 -0400

    Completed the first part of the profile page. Make a new branch for avatars.

--- app/routes.py
+++ app/routes.py
@@ -89,4 +89,4 @@ def user(username):
         {'author':user, 'body':'Test post #1'},
         {'author':user, 'body':'Test post #2'}
     ]
-    
+    return render_template('user.html', user=user, posts=posts)
